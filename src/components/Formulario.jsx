4a04ffd27c9f77585d459eb296fb98cdabbb76cb
Carolina2024 <carcastillos@gmail.com>
2024-05-30 07:29:31 -0400

    componente formulario primera parte

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"; /* se agrega en forma automatica */
+
+/* componente Formulario recibe una prop */
+/* useState devuelve un arreglo, con dos posiciones, el valor y funcion */
+const Formulario = ({ onReg }) => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmarPassword, setConfirmarPassword] = useState("");
+  const [error, setError] = useState("");
+
+  /* funcion validar despues de enviar el formulario */
+  const validar = (e) => {
+    e.preventDefault();
+
+    /* Para validar que campos no esten vacios */
+    if (
+      nombre === "" ||
+      email === "" ||
+      password === "" ||
+      confirmarPassword === ""
+    ) {
+      setError("Completa todos los campos!");
+      onReg({
+        success: false,
+        message: "Completa todos los campos!",
+      });
+      return;
+    }
+
+    /* validar formato email con expresion regular y password que sean iguales */
+    if (!/^\S+@\S+\.\S+/.test(email)) {
+      setError("Email escrito incorrecto");
+      onReg({ success: false, message: "Email escrito incorrecto" });
+      return;
+    }
+    if (password != confirmarPassword) {
+      setError("Password no son iguales");
+      onReg({ success: false, message: "Password no son iguales" });
+      return;
+    }
+
+    /* validar password expresion regular 8 caracteres, al menos una letra, al menos un numero y al menos un simbolo */
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      setError(
+        "Password debe tener minimo 8 caracteres, con letras, numeros y minimo un simbolo"
+      );
+      onReg({
+        success: false,
+        message:
+          "Password debe tener minimo 8 caracteres, con letras, numeros y minimo un simbolo",
+      });
+      return;
+    }
+    setError("");
+
+    /* si se cumple sera exitoso y luego se limpia los campos */
+    onReg({ success: true, message: "Registro realizado" });
+
+    setNombre("");
+    setEmail("");
+    setPassword("");
+    setConfirmarPassword("");
+  };
+
+  /* formulario, validaciones se realizan con funcion validar con evento onSubmit al enviar el formulario */
+  /* onChange para actualizar el estado con valor ingresado */
+
+
+
+
+
+
+
+
+
+};
